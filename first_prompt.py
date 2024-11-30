d5ce6910c21ae3a03274db06e6a7c9e41a0288f8
daneel <49712992+daneelSoftware@users.noreply.github.com>
2024-12-01 00:15:09 +0100

    Add files via upload

--- first_prompt.py
+++ first_prompt.py
@@ -1,25 +1,33 @@
 import os
 import datetime
 import aisuite as ai
+from gtts import gTTS
+
 client = ai.Client()
 
 models = ["openai:gpt-4o", "anthropic:claude-3-5-sonnet-20240620"]
 messages = [
-    {"role": "system", "content": "Respond in German."},
-    {"role": "user", "content": "in what year was Calmbach first mentionned"},
+    {"role": "system", "content": "Respond in french."},
+    {"role": "user", "content": "in what year was Soultzbach-les-bains first mentionned"},
 ]
 now = datetime.datetime.now()
 print ('Begin of program at : ',now.strftime("%Y-%m-%d %H:%M:%S"))
 print ('Models : ',models[0],models[1])
-
 for model in models:
     response = client.chat.completions.create(
         model=model,
         messages=messages,
         temperature=0.75
     )
-    print(response.choices[0].message.content)
+    mytext = response.choices[0].message.content
+    print(mytext)
+    language = 'fr'
+    myobj = gTTS(text=mytext, lang=language, slow=False)
+    myobj.save('welcome.mp3')
+    # read message with mediaplayer
+    os.system('start welcome.mp3')
 
-#os.system('pause')
 now = datetime.datetime.now()
 print ('End of program at : ',now.strftime("%Y-%m-%d %H:%M:%S"))   
+
+   
