ba985680fd3020957268dd5738b4bfadf924087c
daneel <49712992+daneelSoftware@users.noreply.github.com>
2024-12-01 01:05:43 +0100

    Add files via upload

---
+++ read_response.ps1
@@ -0,0 +1,8 @@
+$file='reponse_anthropic.mp3'
+$Mediafile='C:\Users\micro\source\repos\Open_AI\'+$file
+Write-Host $Mediafile
+C:\Users\micro\Documents\PowerShell\Scripts\mp3player.ps1 $Mediafile
+$file='reponse_openai.mp3'
+$Mediafile='C:\Users\micro\source\repos\Open_AI\'+$file
+Write-Host $Mediafile
+C:\Users\micro\Documents\PowerShell\Scripts\mp3player.ps1 $Mediafile
